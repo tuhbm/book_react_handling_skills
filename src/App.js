ddadd85cbc995731b8fd08b78be0e487f1747711
es-tkkim <es-tkkim@smilegate.com>
2023-07-20 16:32:38 +0900

    [MOD]이벤트 핸들링

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, { Fragment } from 'react';
 import MyComponents from './MyComponents';
 import StateComponents from './StateComponents';
 import Say from './Say';
+import EventPractice from './EventPractice';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       </MyComponents>
       <StateComponents/>
       <Say/>
+      <EventPractice />
     </Fragment>
   );
 }
